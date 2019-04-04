d88d14ea943e83367d456d3a0d8e55d3723138e0
piotsik <36536181+piotsik@users.noreply.github.com>
2019-04-05 00:14:33 +0200

    Finished

--- nm3.py
+++ nm3.py
@@ -1,53 +1,60 @@
-def f(x):
-    return 3 * (x ** 2) + 4 * x - 4
+import numpy as np
 
 
-def nodes():
+def f(x):  # function
+    return 3 * (x ** 2) - 2 * x - 5
+    # return np.cos(x)
+
+
+def nodes():  # equally spaced nodes
     h = (b - a) / nc
-    for nn in range(nc):
+    for nn in range(nc + 1):
         n.append(a + nn * h)
-    print(n)
 
 
-def cheb():
-    h = (1.0 - (-1.0)) / nc
-    for nn in range(nc):
-        n.append(((b - a) * (-1 + h * nn) / 2) + a + b)
-    print(n)
+def xi(i):  # chebyshev function
+    return np.cos((2 * i + 1) * np.pi / (2 * nc + 2))
 
 
-def errors():
+def cheb():  # chebyshev nodes
+    for i in range(nc + 1):
+        n[nc - i] = ((b - a) * xi(i) / 2) + (a + b) / 2
+    print n
+
+
+def errors(o):  # calculate errors with step o
     err = []
-    for nn in range(nc):
-        err.append(f(n[nn]) - w(n[nn]))
-    print(err)
+    for v in np.arange(a, b, o):
+        err.append(abs(f(v) - w(v)))
+    return max(err), f(v), v
 
 
-def aux(i, x):
-    w = 1
-    for j in range(nc):
-        if j != i:
-            w *= (x - n[j]) / (n[i] - n[j])
-    return w
-
-
-def w(x):
-    result = 0
-    for m in range(nc):
-        result += aux(m, e) * f(n[m])
+def w(x):  # interpolation value for x
+    aux = np.zeros(nc + 1)
+    result = 0.0
+    for m in range(nc + 1):
+        chunk = 1.0
+        for j in range(nc + 1):
+            if j != m:
+                chunk *= (x - n[j]) / (n[m] - n[j])
+        aux[m] = chunk
+    for i in range(nc + 1):
+        result += f(n[i]) * aux[i]
     return result
 
 
 n = []
-nc = input("How many nodes: ")
+nc = input("Order: ")
 a = float(input("Lower bound: "))
 b = float(input("Upper bound: "))
 e = input("Point: ")
+o = 0.01
+
+nodes()
+print(n)
 if input("Type '1' to use Chebyshev nodes. Anything else if not: ") == 1:
     cheb()
-else:
-    nodes()
 
 print("Value of interpolated function at " + str(e) + " equals " + str(w(e)))
-errors()
-print(f(e) - w(e))
+print("Max error / value of max error / x of max error: ")
+print(errors(o))
