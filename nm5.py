aa800239e66975456781bc244347776a26f29c91
piotsik <36536181+piotsik@users.noreply.github.com>
2019-04-15 17:50:05 +0200

    Fixes

--- nm5.py
+++ nm5.py
@@ -1,72 +1,75 @@
-#Basic functions
-import math
-
+import math as m
 
 
 def big_F(x):
-    return (((x**4)/2)+math.sin(x))
-   #return (((x ** 4) / 8) + ((x ** 3) / 2))
+    return (((x ** 4) / 2) + m.sin(x))
+    # return (((x ** 4) / 8) + ((x ** 3) / 2))
 
 
 def small_F(x):
-    return (2*(x**3)+math.cos(x))
-    #return (((x**3)+(3*(x**2)))/2)
-
+    return (2 * (x ** 3) + m.cos(x))
+    # return (((x ** 3) + (3 * (x ** 2))) / 2)
 
 
 def Analitical(a, b):
     return big_F(b) - big_F(a)
 
-# Simple approximation of area under the curve - constant line y=midPoint is  (n=0)
 
-def areaUnderCurve(a,b,h):
-    midPoint=(a+b)/2
-    value=h*small_F(midPoint)
-    error=Analitical(a,b)-value
-    print('Value: ',value)
-    print('Error: ',error)
+"""
+Simple approximation of area under the curve -
+constant line y = midPoint is (n = 0)
+"""
 
-# Simple Trapezoid Method (n=1)
+
+def areaUnderCurve(a, b, h):
+    midPoint = (a + b) / 2
+    value = h * small_F(midPoint)
+    error = Analitical(a, b) - value
+    print('Value: ' + str(value))
+    print('Error: ' + str(error))
+
+
+# Simple Trapezoid Method (n = 1)
 def simpleTrapezoid(a, b, h):
     value = ((h/2) * (small_F(a) + small_F(b)))
 
-#to change ->
+    # to change ->
     zeta = -0.721
 
-    secondDerivative = ((12*zeta)-(math.cos(zeta)))
+    secondDerivative = ((12 * zeta) - (m.cos(zeta)))
 
     error = (((h ** 3) * secondDerivative) / -12)
-    print('Value: ',value)
-    print('Error: ',error)
+    print('Value: ' + str(value))
+    print('Error: ' + str(error))
 
 
-# Simple Simpson Formula (n=2)
+# Simple Simpson Formula (n = 2)
 def simpleSimpson(a, b, h):
     h = h / 2
     x1 = (a + b) / 2.0
     value = ((h * (1.0 / 3.0)) * (small_F(a) + 4 * (small_F(x1)) + small_F(b)))
 
-    #to change ->
-    zeta=0.5
-    fourthDerivative = math.cos(zeta)
+    # to change ->
+    zeta = 0.5
+    fourthDerivative = m.cos(zeta)
 
     error = ((-1 / 90) * (h ** 5) * fourthDerivative)
-    print('Value: ',value)
-    print('Error: ',error)
+    print('Value: ' + str(value))
+    print('Error: ' + str(error))
 
 
-# Simple Cheb. Method (n=2)
+# Simple Cheb. Method (n = 2)
 def simpleCheb_n2(a, b, h):
     t = 0.577350
     x1 = (b + a) / 2 + (b - a) / 2 * (-t)
     x2 = (b + a) / 2 + (b - a) / 2 * t
     value = ((h / 2) * (small_F(x1) + small_F(x2)))
     error = Analitical(a, b) - value
-    print('Value: ',value)
-    print('Error: ',error)
+    print('Value: ' + str(value))
+    print('Error: ' + str(error))
 
 
-# Simple Cheb. Method (n=4)
+# Simple Cheb. Method (n = 4)
 def simpleCheb_n4(a, b, h):
     t1 = 0.794654
     t2 = 0.187592
@@ -77,23 +80,21 @@ def simpleCheb_n4(a, b, h):
 
     value = (h / 4) * (small_F(x1) + small_F(x2) + small_F(x3) + small_F(x4))
     error = Analitical(a, b) - value
-    print('Value: ',value)
-    print('Error: ',error)
+    print('Value: ' + str(value))
+    print('Error: ' + str(error))
 
 
 a = -1.0
 b = 0.5
-h=(b-a)
-print('GitHub')
-
+h = (b - a)
 
 print('\nSimple Area under curve: ')
-areaUnderCurve(a,b,h)
+areaUnderCurve(a, b, h)
 print('\nSimple Trapezoid: ')
 simpleTrapezoid(a, b, h)
 print('\nSimple Simpson: ')
 simpleSimpson(a, b, h)
-print('\nSimple Cheb. n=2: ')
+print('\nSimple Cheb. n = 2: ')
 simpleCheb_n2(a, b, h)
-print('\nSimple Cheb. n=4: ')
+print('\nSimple Cheb. n = 4: ')
 simpleCheb_n4(a, b, h)
