8759d2d6bc1681182b30152e361ba05429c4a69f
piotsik <36536181+piotsik@users.noreply.github.com>
2019-04-29 16:00:09 +0200

    Corrected

--- nm5.py
+++ nm5.py
@@ -1,14 +1,13 @@
 import math as mth
 
+
 # Main functions used
 def big_F(x):
     return ((x ** 4) / 2) + mth.sin(x)
-    
 
 
 def small_F(x):
     return 2 * (x ** 3) + mth.cos(x)
-    
 
 
 def Analitical(a, b):
@@ -33,7 +32,7 @@ def simpleTrapezoid(a, b):
     h = b - a
     value = (h / 2) * (small_F(a) + small_F(b))
     error = Analitical(a, b) - value
-    
+
     print("Value: " + str(value))
     print("Error: " + str(error))
 
@@ -89,75 +88,84 @@ def simpleCheb_n4(a, b):
 # Composite Trapezoid Formula (n=1, m=4)
 def compTrapezoid(a, b):
 
-    m=4 
-    distance=(a-b)/(m+1)
-    x1=a+abs(distance)
-    x2=x1+abs(distance)
-    x3=x2+abs(distance)
-    x4=x3+abs(distance)
+    m = 4
+    distance = (a - b) / (m + 1)
+    x1 = a + abs(distance)
+    x2 = x1 + abs(distance)
+    x3 = x2 + abs(distance)
+    x4 = x3 + abs(distance)
 
-    value=(((b-a)/(2*m))*(small_F(a)+small_F(b)+2*small_F(x1)+2*small_F(x2)+2*small_F(x3)+2*small_F(x4)))
-    error=Analitical(a,b)-value
+    value = ((b - a) / (2 * m)) * (
+        small_F(a) +
+        small_F(b) +
+        2 * small_F(x1) +
+        2 * small_F(x2) +
+        2 * small_F(x3) +
+        2 * small_F(x4)
+    )
+    error = Analitical(a, b) - value
 
     print("Value: " + str(value))
     print("Error: " + str(error))
 
 
 # Composite Simpson Formula (n=2, m=2)
-def compSimpson(a,b):
-  
-    m=2
-    distance=(a-b)/(2*m)
-    x1=a+abs(distance)
-    x2=x1+abs(distance)
-    x3=x2+abs(distance)
+def compSimpson(a, b):
 
-    value=((abs(distance)/-3)*(small_F(a)+4*small_F(x1)+2*small_F(x2)+4*small_F(x3)+small_F(b)))
+    m = 2
+    distance = (a - b) / (2 * m)
+    x1 = a + abs(distance)
+    x2 = x1 + abs(distance)
+    x3 = x2 + abs(distance)
+
+    value = (abs(distance) / -3) * (
+        small_F(a) + 4 * small_F(x1) + 2 * small_F(x2) +
+        4 * small_F(x3) + small_F(b)
+    )
 
     # to change ->
     zeta = 0.5
     fourthDeriv = mth.cos(zeta)
     error = (-1 / 90) * m * (distance ** 5) * fourthDeriv
-  
+
     print("Value: " + str(value))
     print("Error: " + str(error))
 
 
 # Composite Rectangle Method (n=0, m=20)
-def compRectangle(a,b):
+def compRectangle(a, b):
 
     # Switch between m=4 and m=20
-    m=20
+    m = 20
 
-    value=0.0
+    value = 0.0
 
-    distance=(b-a)/(m+1)
+    distance = (b - a) / (m + 1)
 
-    node=a
-    nodes=[]
+    node = a
+    nodes = []
     nodes.append(node)
-  
-    for i in range(0,m+1):
-      node=node+distance
-      nodes.append(node)
 
-    middlePoints=[]
+    for i in range(0, m + 1):
+        node = node + distance
+        nodes.append(node)
 
-    for i in range(0,m+1):
-      middlePoint=(nodes[i]+nodes[i+1])/2
-      middlePoints.append(middlePoint)
+    middlePoints = []
 
-    mpValues=[]
+    for i in range(0, m + 1):
+        middlePoint = (nodes[i] + nodes[i + 1]) / 2
+        middlePoints.append(middlePoint)
 
-    for i in range(0,m+1):
-      mpValue=distance*(small_F(middlePoints[i]))
-      mpValues.append(mpValue)
+    mpValues = []
 
-    
-    for i in range(0,m+1):
-      value=value+mpValues[i]
+    for i in range(0, m + 1):
+        mpValue = distance * (small_F(middlePoints[i]))
+        mpValues.append(mpValue)
 
-    error=Analitical(a,b)-value
+    for i in range(0, m + 1):
+        value = value + mpValues[i]
+
+    error = Analitical(a, b) - value
 
     print("Value: " + str(value))
     print("Error: " + str(error))
@@ -182,13 +190,13 @@ print("\nSimple Cheb. n = 2: ")
 simpleCheb_n2(a, b)
 
 print("\nSimple Cheb. n = 4: ")
-simpleCheb_n4(a, b,)
+simpleCheb_n4(a, b)
 
 print("\nComposite Trapezoid: ")
-compTrapezoid(a,b)
+compTrapezoid(a, b)
 
 print("\nComposite Simpson: ")
-compSimpson(a,b)
+compSimpson(a, b)
 
 print("\nComposite Rectangle: ")
-compRectangle(a,b)
+compRectangle(a, b)
