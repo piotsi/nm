a829d9c3e59174e12f056c49dd986e72f5f50e12
wojpon <wojtek.ponikiewski@gmail.com>
2019-05-15 18:08:15 +0200

    Finished

--- nm10.py
+++ nm10.py
@@ -12,21 +12,25 @@ def function2(x, y):
 def euler(h, interval):
 
     a, b = interval
+    x0 = a
     y0 = 5
+
+    print("\nEuler results: \n")
+
     print(
-        "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
-            0, a, y0, function1(a, y0), 0
+        "i: {}      xi: {:.5f} yi: {:.5f}".format(
+            0, x0, y0
         )
     )
     for i in range(1, int(b / h) + 1):
-        delta = h * function1(a, y0)
+        delta = h * function1(x0, y0)
         y = y0 + h * delta
         x = i * h
         y0 = y
-        a = x
+        x0 = x
         print(
-            "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
-                i, a, y0, function1(a, y0), delta
+            "i: {}      xi: {:.5f} yi: {:.5f}".format(
+                i, x0, y0
             )
         )
 
@@ -34,32 +38,36 @@ def euler(h, interval):
 def runge_kutta(h, interval):
     a, b = interval
     x0 = a
-    y0 = 0
+    y0 = 5
+
+    print("\nRunge-Kutta results: \n")
+
     print(
-        "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
-            0, x0, y0, function2(x0, y0), 0
+        "i: {}      xi: {:.5f} yi: {:.5f}".format(
+            0, x0, y0
         )
     )
 
     for i in range(1, int(b / h) + 1):
 
-        k1 = h * function2(x0, y0)
-        k2 = h * function2(x0 + h / 2, y0 + k1 / 2)
-        k3 = h * function2(x0 + h / 2, y0 + k2 / 2)
-        k4 = h * function2(x0 + h, y0 + k3)
+        k1 = h * function1(x0, y0)
+        k2 = h * function1(x0 + h / 2, y0 + k1 / 2)
+        k3 = h * function1(x0 + h / 2, y0 + k2 / 2)
+        k4 = h * function1(x0 + h, y0 + k3)
 
         delta = (k1 + 2 * k2 + 2 * k3 + k4) / 6
         y = y0 + delta
         x = i * h
         y0 = y
+        x0 = x
         print(
-            "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
-                i, x0, y0, function2(x0, y0), delta
+            "i: {}      xi: {:.5f} yi: {:.5f}".format(
+                i, x0, y0
             )
         )
 
 
 h = 1
-interval = (0, 5)
-# euler(h, interval)
+interval = (0, 20)
+euler(h, interval)
 runge_kutta(h, interval)
