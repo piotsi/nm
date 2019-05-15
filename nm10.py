a64fe3831f03a746c5d04320f423d047a5d48a33
piotsik <36536181+piotsik@users.noreply.github.com>
2019-05-15 12:04:45 +0200

    Half done

---
+++ nm10.py
@@ -0,0 +1,65 @@
+import math as m
+
+
+def function1(x, y):
+    return -0.7 * y + m.e ** -x
+
+
+def function2(x, y):
+    return 7.0 * y + 14
+
+
+def euler(h, interval):
+
+    a, b = interval
+    y0 = 5
+    print(
+        "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
+            0, a, y0, function1(a, y0), 0
+        )
+    )
+    for i in range(1, int(b / h) + 1):
+        delta = h * function1(a, y0)
+        y = y0 + h * delta
+        x = i * h
+        y0 = y
+        a = x
+        print(
+            "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
+                i, a, y0, function1(a, y0), delta
+            )
+        )
+
+
+def runge_kutta(h, interval):
+    a, b = interval
+    x0 = a
+    y0 = 0
+    print(
+        "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
+            0, x0, y0, function2(x0, y0), 0
+        )
+    )
+
+    for i in range(1, int(b / h) + 1):
+
+        k1 = h * function2(x0, y0)
+        k2 = h * function2(x0 + h / 2, y0 + k1 / 2)
+        k3 = h * function2(x0 + h / 2, y0 + k2 / 2)
+        k4 = h * function2(x0 + h, y0 + k3)
+
+        delta = (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        y = y0 + delta
+        x = i * h
+        y0 = y
+        print(
+            "i: {} xi:{:.5f} yi:{:.5f} f:{:.5f} d:{:.5f}".format(
+                i, x0, y0, function2(x0, y0), delta
+            )
+        )
+
+
+h = 1
+interval = (0, 5)
+# euler(h, interval)
+runge_kutta(h, interval)
