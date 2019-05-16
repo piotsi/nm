dccf0e2a693a0d3a1485d18338f8752f75cd9711
piotsik <36536181+piotsik@users.noreply.github.com>
2019-05-16 21:54:08 +0200

    Done

--- nm10.py
+++ nm10.py
@@ -2,82 +2,68 @@ import math as m
 from matplotlib import pyplot as plt
 
 
-def function1(x, y):
+def f(x, y):
     return -0.7 * y + m.e ** -x
 
 
-def euler(h, interval):
-
-    a, b = interval
-    x0 = a
-    y0 = 5
-    xiTab = [0]
-    yiTab = [5]
+def euler(h, interval, y0):
+    x0, b = interval
+    xiTab, yiTab = [], []
 
     print("\nEuler results: \n")
 
-    print(
-        "i: {}      xi: {:.5f} yi: {:.5f}".format(
-            0, x0, y0
-        )
-    )
-    for i in range(1, int(b / h) + 1):
-        delta = h * function1(x0, y0)
-        y = y0 + h * delta
-        x = i * h
-        yiTab.append(y)
-        y0 = y
-        x0 = x
-        xiTab.append(x0)
+    for i in range(int(b / h) + 1):
         print(
-            "i: {}      xi: {:.5f} yi: {:.5f}".format(
+            "i: {:3} xi: {:8.5f} yi: {:8.5f}".format(
                 i, x0, y0
             )
         )
-    plt.plot(xiTab, yiTab)
-    plt.show()
+
+        delta = h * f(x0, y0)
+
+        y0 = y0 + h * delta
+        x0 = i * h
+
+        xiTab.append(x0)
+        yiTab.append(y0)
+
+    plt.plot(xiTab, yiTab, 'bo-', label='Euler')
 
 
-def runge_kutta(h, interval):
-    a, b = interval
-    x0 = a
-    y0 = 5
-    xiTab = [0]
-    yiTab = [5]
+def runge_kutta(h, interval, y0):
+    x0, b = interval
+    xiTab, yiTab = [], []
 
     print("\nRunge-Kutta results: \n")
 
-    print(
-        "i: {}      xi: {:.5f} yi: {:.5f}".format(
-            0, x0, y0
-        )
-    )
-
-    for i in range(1, int(b / h) + 1):
-
-        k1 = h * function1(x0, y0)
-        k2 = h * function1(x0 + h / 2, y0 + k1 / 2)
-        k3 = h * function1(x0 + h / 2, y0 + k2 / 2)
-        k4 = h * function1(x0 + h, y0 + k3)
-
-        delta = (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        y = y0 + delta
-        yiTab.append(y)
-
-        x = i * h
-        y0 = y
-        x0 = x
-        xiTab.append(x0)
+    for i in range(int(b / h) + 1):
         print(
-            "i: {}      xi: {:.5f} yi: {:.5f}".format(
+            "i: {:3} xi: {:8.5f} yi: {:8.5f}".format(
                 i, x0, y0
             )
         )
-    plt.plot(xiTab, yiTab)
-    plt.show()
+        
+        k1 = h * f(x0, y0)
+        k2 = h * f(x0 + h / 2, y0 + k1 / 2)
+        k3 = h * f(x0 + h / 2, y0 + k2 / 2)
+        k4 = h * f(x0 + h, y0 + k3)
+
+        delta = (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+        y0 = y0 + delta
+        x0 = i * h
+
+        xiTab.append(x0)
+        yiTab.append(y0)
+
+    plt.plot(xiTab, yiTab, 'ro-', label='Range-Kutta')
 
 
 h = 1
 interval = (0, 20)
-euler(h, interval)
-runge_kutta(h, interval)
+y0 = 5
+euler(h, interval, y0)
+runge_kutta(h, interval, y0)
+plt.legend(loc='upper right')
+plt.axhline(color='k', linewidth=0.5)
+plt.show()
